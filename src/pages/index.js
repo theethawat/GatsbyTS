aa894e2b0bdd964341f2e5dd1d8d2840accf8132
Theethawat <5910110150@psu.ac.th>
2019-05-12 10:18:56 +0700

    updating index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap"
 import "../assets/global.css"
-import Header from "../components/header"
+import Title from "../components/title"
 import { Link } from "gatsby"
 export default () => (
   <div>
-    <Header headerText="Theethawat Savastham" />
-    <div className="container">
-      <h3 className="kanit ">ธีร์ธวัช สวาสดิ์ธรรม</h3>
-      <h4 className="">
-        Computer Engineering Student, Prince of Songkla University
-      </h4>
+    <Title
+      pagetitle="Theethawat Savastham"
+      subtitle="<TheethawatSpace is the space of 'ธีร์ธวัช สวาสดิ์ธรรม'/> "
+    />
 
-      <h2>Hello Tin </h2>
+    <div className="container">
+      <h3 className="kanit ">What is Theethawat ?</h3>
+      <div className="row">
+        <div className="col sm-9">
+          <div className="row">
+            <div className="col sm-9">
+              <blockquote className="blockquote">
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer posuere erat a ante.
+                </p>
+              </blockquote>
+            </div>
+            <div className="col sm-3" />
+          </div>
+        </div>
+        <div className="col sm-3" />
+      </div>
     </div>
   </div>
 )
